992809f1d48044e1d8cbd252b3ea365c36670a11
NicholasD6969 <nicholasdaniel606@gmail.com>
2025-05-22 18:38:51 +0000

    js

---
+++ 1.js/5.switch_case.js
@@ -0,0 +1,19 @@
+const opcao = 2
+
+switch (opcao) {
+    case 1:
+        console.log('Opção 1')
+        break;
+    case 2:
+        console.log('Opção 2')
+        break;
+    case 3:
+        console.log('Opção 3')
+        break;
+    case 4:
+        console.log('Opção 4')
+        break;
+    default:
+        console.log('Opção invalída.')
+        break    
+}
